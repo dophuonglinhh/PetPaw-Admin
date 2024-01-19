230528d0407cd870f065a5e8c5943e9cd21a1d0d
leQwang <s3912105@rmit.edu.vn>
2024-01-19 16:45:55 +0700

    [UPDATE] add home page admin

--- app/src/main/java/com/example/petpawadmin/fragments/screens/HomeFragment.java
+++ app/src/main/java/com/example/petpawadmin/fragments/screens/HomeFragment.java
@@ -7,8 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 
 import com.example.petpawadmin.R;
+import com.example.petpawadmin.databinding.FragmentHomeBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -26,6 +31,7 @@ public class HomeFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+    private FragmentHomeBinding binding;
     public HomeFragment() {
         // Required empty public constructor
     }
@@ -60,7 +66,42 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        binding= FragmentHomeBinding.inflate(inflater, container, false);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("users").get().addOnSuccessListener(queryDocumentSnapshots -> {
+            int usersCount = queryDocumentSnapshots.size();
+            binding.adminHomeUsersTextView.setText(String.valueOf(usersCount));
+        });
+
+        db.collection("Communities").get().addOnSuccessListener(queryDocumentSnapshots -> {
+            int communitiesCount = queryDocumentSnapshots.size();
+            binding.adminHomeCommunitiesTextView.setText(String.valueOf(communitiesCount));
+        });
+
+        db.collection("Posts").get().addOnSuccessListener(queryDocumentSnapshots -> {
+            int postsCount = queryDocumentSnapshots.size();
+            binding.adminHomePostsTextView.setText(String.valueOf(postsCount));
+        });
+
+        db.collection("Pets").get().addOnSuccessListener(queryDocumentSnapshots -> {
+            int petsCount = queryDocumentSnapshots.size();
+            binding.adminHomePetsTextView.setText(String.valueOf(petsCount));
+        });
+
+        //display the listview
+        db.collection("Admin").document("nolD8tefH4w9mwE9efzM").get().addOnSuccessListener(documentSnapshot -> {
+            List<String> adminNotifications = (List<String>) documentSnapshot.get("notifications");
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                    requireContext(),
+                    android.R.layout.simple_list_item_1,
+                    adminNotifications
+            );
+
+            binding.adminHomeNotificationListView.setAdapter(adapter);
+        });
+
+        return binding.getRoot();
     }
 }
