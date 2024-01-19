f32a6975a4d9b95c93e76fb94300a863bceb8e1c
dophuonglinhh <s3926823@rmit.edu.vn>
2024-01-19 11:01:20 +0700

    update xml

--- app/src/main/java/com/example/petpawadmin/fragments/screens/UsersFragment.java
+++ app/src/main/java/com/example/petpawadmin/fragments/screens/UsersFragment.java
@@ -17,8 +17,6 @@ import com.example.petpawadmin.R;
  */
 public class UsersFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
